8924cf6ec0de09b754e8e7af655ef2c9fff7fdf0
anhnlt0543 <118040137+anhnlt0543@users.noreply.github.com>
2022-11-17 00:02:07 +0700

    SPA

---
+++ QuanLyCuaHangGiaySportPowers/src/Services/SanPham_Interface.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Services;
+
+
+
+import ViewModels.SanPham_ViewModel;
+
+import java.util.List;
+
+/**
+ *
+ * @author TUAN ANH
+ */
+public interface SanPham_Interface {
+    List<SanPham_ViewModel> getAll();
+    
+    SanPham_ViewModel addModel(SanPham_ViewModel sp);
+    SanPham_ViewModel DeleteModel(String idSP);
+    SanPham_ViewModel UpdateModel(SanPham_ViewModel sp,String idSP);
+    
+    
+    
+}
